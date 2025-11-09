7c7e0c14b0375a02b7e41eb6f92c60ad508615a7
Dzmitry Varanetski <dimavo2005@gmail.com>
2025-11-09 15:09:21 +0100

    Create Customer class with inheritance from Person Implement simple email validation and password hashing (Not yet implemented: bonus points calculation)

---
+++ src/Cinema.Core/models/Customer.cs
@@ -0,0 +1,77 @@
+using System.Net.Mail;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Cinema.Core.models;
+
+public class Customer : Person
+{
+    public string Email { get; private set; }
+    public string HashPassword { get; private set; }
+    public int BonusPoints { get; private set; }
+
+    public Customer(string firstName, string lastName, DateOnly dateOfBirth,
+        string email, string rawPassword, int bonusPoints)
+        : base(firstName, lastName, dateOfBirth)
+    {
+        if (string.IsNullOrWhiteSpace(email))
+            throw new ArgumentException("Email cannot be empty.");
+
+        if (!EmailIsValid(email))
+            throw new ArgumentException("Invalid email format.");
+
+        if (string.IsNullOrWhiteSpace(rawPassword))
+            throw new ArgumentException("Password cannot be empty.");
+
+        if (bonusPoints < 0)
+            throw new ArgumentException("Bonus points cannot be negative.");
+        
+        if (rawPassword.Length < 6)
+            throw new ArgumentException("Raw password cannot be less than 6 characters.");
+
+        Email = email;
+        HashPassword = HashPasswordEncoder(rawPassword);
+        BonusPoints = bonusPoints;
+    }
+
+    public void AddBonusPoints(int points)
+    {
+        if (points < 0)
+            throw new ArgumentException("Cannot add negative bonus points.");
+        BonusPoints += points;
+    }
+    
+    public void RemoveBonusPoints(int points)
+    {
+        if (points < 0)
+            throw new ArgumentException("Cannot remove a negative number of points.");
+
+        if (points > BonusPoints)
+            throw new ArgumentException("Not enough bonus points to remove.");
+
+        BonusPoints -= points;
+    }
+    
+
+    private string HashPasswordEncoder(string password)
+    {
+        using (SHA256 sha256 = SHA256.Create())
+        {
+            byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+            return Convert.ToBase64String(bytes);
+        }
+    }
+
+    private bool EmailIsValid(string email)
+    {
+        try
+        {
+            var addr = new MailAddress(email);
+            return addr.Address == email;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+}
