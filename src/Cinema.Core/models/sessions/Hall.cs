f05ca972778d933ea6257aa3ebbe1d9451869b57
Dzmitry Varanetski <dimavo2005@gmail.com>
2025-11-14 13:12:06 +0100

    .

--- src/Cinema.Core/models/sessions/Hall.cs
+++ src/Cinema.Core/models/sessions/Hall.cs
@@ -4,6 +4,8 @@ namespace Cinema.Core.models.sessions;
 
 public class Hall
 {
+    
+    private static int _MAX_CAPACITY = 150;
     private static int _counter = 0;
     
     public static List<Hall> All { get; } = new();
@@ -12,7 +14,7 @@ public class Hall
     
     public string Name { get; private set; }
     
-    public int MaxCapacity { get; private set; }
+    public int Capacity { get; private set; }
     
     private readonly Dictionary<int, Seat> _seatsByNumber = new();
     public IReadOnlyCollection<Seat> Seats => _seatsByNumber.Values;
@@ -23,16 +25,16 @@ public class Hall
     private readonly List<Movie> _movies = new();
     public IReadOnlyList<Movie> Movies => _movies.AsReadOnly();
     
-    public Hall(string name, int maxCapacity = 150)
+    public Hall(string name, int capacity)
     {
         if (string.IsNullOrWhiteSpace(name))
             throw new ArgumentException("Hall name cannot be empty.", nameof(name));
-        if (maxCapacity <= 0)
-            throw new ArgumentException("Capacity must be positive.", nameof(maxCapacity));
+        if (capacity <= 0 || capacity > _MAX_CAPACITY)
+            throw new ArgumentException("Capacity must be positive or lower than ", nameof(_MAX_CAPACITY));
 
         Id = ++_counter;
         Name = name;
-        this.MaxCapacity = maxCapacity;
+        Capacity  = capacity;
         
         All.Add(this);
     }
@@ -49,9 +51,9 @@ public class Hall
             throw new InvalidOperationException(
                 $"Seat number {seatNumber} already exists in hall {Name}.");
 
-        if (_seatsByNumber.Count >= MaxCapacity)
+        if (_seatsByNumber.Count >= _MAX_CAPACITY)
             throw new InvalidOperationException(
-                $"Hall {Name} is at full capacity ({MaxCapacity} seats).");
+                $"Hall {Name} is at full capacity ({_MAX_CAPACITY} seats).");
 
         _seatsByNumber[seatNumber] = seat;
     }
